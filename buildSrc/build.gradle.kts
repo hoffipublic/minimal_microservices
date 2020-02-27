daa83b622e57f15dca7389800731015cba21a7ee
Dirk Hoffmann (Dell EMC) <Dirk.Hoffmann@dell.com>
2020-02-27 09:54:21 +0100

    linked buildSrc/build.gradle.kts to ../../gradle_dependencies/buildSrc/build.gradle.kts

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,12 +1,1 @@
-plugins {
-    `kotlin-dsl`
-}
-
-repositories {
-    mavenLocal()
-    jcenter()
-}
-
-dependencies {
-    implementation("org.yaml:snakeyaml:1.25") // https://bitbucket.org/asomov/snakeyaml/wiki/Documentation
-}
+../../gradle_dependencies/buildSrc/build.gradle.kts
